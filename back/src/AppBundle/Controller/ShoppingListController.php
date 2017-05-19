f4c0c18a86e8ab3b3d4a505ddbedc909f9a626fb
Metallou <kevincastejon13@gmail.com>
2017-05-19 15:49:00 +0200

    most back routes defined

--- back/src/AppBundle/Controller/ShoppingListController.php
+++ back/src/AppBundle/Controller/ShoppingListController.php
@@ -5,7 +5,9 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\ShoppingList;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Shoppinglist controller.
@@ -14,123 +16,154 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component
  */
 class ShoppingListController extends Controller
 {
-    /**
-     * Lists all shoppingList entities.
-     *
-     * @Route("/", name="shoppinglist_index")
-     * @Method("GET")
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
+  /**
+   * Lists all shoppingList entities.
+   *
+   * @Route("/{userID}/show", name="shoppinglist_index")
+   * @Method("GET")
+   */
+  public function indexAction(Request $request, $userID)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $shoppingLists = $em->getRepository('AppBundle:ShoppingList')->findAll();
 
-        $shoppingLists = $em->getRepository('AppBundle:ShoppingList')->findAll();
-
-        return $this->render('shoppinglist/index.html.twig', array(
-            'shoppingLists' => $shoppingLists,
-        ));
+    $show = array();
+    foreach($shoppingLists as $shoppingList) {
+      $tmp = array("id" => $shoppingList->getId(), "name" => $shoppingList->getName());
+      array_push($show, $tmp);
     }
 
-    /**
-     * Creates a new shoppingList entity.
-     *
-     * @Route("/new", name="shoppinglist_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-        $shoppingList = new Shoppinglist();
-        $form = $this->createForm('AppBundle\Form\ShoppingListType', $shoppingList);
-        $form->handleRequest($request);
+    return new JsonResponse(array('user' => $userID, 'shoppingLists' => $show));
+  }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($shoppingList);
-            $em->flush();
-
-            return $this->redirectToRoute('shoppinglist_show', array('id' => $shoppingList->getId()));
-        }
-
-        return $this->render('shoppinglist/new.html.twig', array(
-            'shoppingList' => $shoppingList,
-            'form' => $form->createView(),
-        ));
+  /**
+   * Creates a new shoppingList entity.
+   *
+   * @Route("/{userID}/new", name="shoppinglist_new")
+   * @Method({"GET", "POST"})
+   */
+  public function newAction(Request $request, $userID)
+  {
+    if($userID<1) {
+      return new JsonResponse(array('error' => 'user'));
+    }
+    $em = $this->getDoctrine()->getManager();
+    $user = $em->getRepository('AppBundle:User')->find($userID);
+    if($user==NULL) {
+      return new JsonResponse(array('error' => 'user'));
     }
 
-    /**
-     * Finds and displays a shoppingList entity.
-     *
-     * @Route("/{id}", name="shoppinglist_show")
-     * @Method("GET")
-     */
-    public function showAction(ShoppingList $shoppingList)
-    {
-        $deleteForm = $this->createDeleteForm($shoppingList);
+    $shoppingList = new Shoppinglist($user, "Coucou");
+    $em->persist($shoppingList);
+    $em->flush();
 
-        return $this->render('shoppinglist/show.html.twig', array(
-            'shoppingList' => $shoppingList,
-            'delete_form' => $deleteForm->createView(),
-        ));
+    return $this->redirectToRoute('shoppinglist_index', array('userID' => $userID));
+  }
+
+  /**
+   * Finds and displays a shoppingList entity
+   *
+   * @Route("/{userID}/{shoppingListID}/show", name="shoppinglist_show")
+   * @Method("GET")
+   */
+  public function showAction(Request $request, $userID, $shoppingListID)
+  {
+    if($userID<1) {
+      return new JsonResponse(array('error' => 'user'));
+    }
+    if($shoppingListID<1) {
+      return new JsonResponse(array('error' => 'shoppingList'));
+    }
+    $em = $this->getDoctrine()->getManager();
+    $user = $em->getRepository('AppBundle:User')->find($userID);
+    if($user==NULL) {
+      return new JsonResponse(array('error' => 'user'));
+    }
+    $shoppingLists = $em->getRepository('AppBundle:ShoppingList')->findByUser($userID);
+    if($shoppingListID>count($shoppingLists)) {
+      return new JsonResponse(array('error' => 'shoppingList'));
+    }
+    $shoppingList = $shoppingLists[$shoppingListID-1];
+
+    $tmp = array();
+    foreach($shoppingList->getProducts() as $product) {
+      array_push($tmp, array(
+        "id" => $product->getId(),
+        "name" => $product->getName(),
+        "quantity" => $product->getQuantity(),
+        "bought" => $product->getBought()));
+    }
+    $show = array(
+      "id" => $shoppingList->getId(),
+      "name" => $shoppingList->getName(),
+      "products" => $tmp);
+
+    return new JsonResponse(array('user' => $userID, 'shoppingList' => $show));
+  }
+
+  /**
+   * Displays a form to edit an existing shoppingList entity.
+   *
+   * @Route("/{userID}/{shoppingListID}/edit", name="shoppinglist_edit")
+   * @Method({"GET", "POST"})
+   */
+  public function editAction(Request $request, $userID, $shoppingListID)
+  {
+    if($userID<1) {
+      return new JsonResponse(array('error' => 'user'));
+    }
+    if($shoppingListID<1) {
+      return new JsonResponse(array('error' => 'shoppingList'));
+    }
+    $em = $this->getDoctrine()->getManager();
+    $user = $em->getRepository('AppBundle:User')->find($userID);
+    if($user==NULL) {
+      return new JsonResponse(array('error' => 'user'));
+    }
+    $shoppingLists = $em->getRepository('AppBundle:ShoppingList')->findByUser($userID);
+    if($shoppingListID>count($shoppingLists)) {
+      return new JsonResponse(array('error' => 'shoppingList'));
+    }
+    $shoppingList = $shoppingLists[$shoppingListID-1];
+
+    if (false) {
+      //$shoppingList = $editForm->getData();
+      $em->persist($shoppingList);
+      $this->getDoctrine()->getManager()->flush();
     }
 
-    /**
-     * Displays a form to edit an existing shoppingList entity.
-     *
-     * @Route("/{id}/edit", name="shoppinglist_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, ShoppingList $shoppingList)
-    {
-        $deleteForm = $this->createDeleteForm($shoppingList);
-        $editForm = $this->createForm('AppBundle\Form\ShoppingListType', $shoppingList);
-        $editForm->handleRequest($request);
+    return new JsonResponse(array('shoppingList' => $shoppingList));
+  }
 
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('shoppinglist_edit', array('id' => $shoppingList->getId()));
-        }
-
-        return $this->render('shoppinglist/edit.html.twig', array(
-            'shoppingList' => $shoppingList,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
+  /**
+   * Deletes a shoppingList entity.
+   *
+   * @Route("/{userID}/{shoppingListID}/delete", name="shoppinglist_delete")
+   * @Method("GET")
+   */
+  public function deleteAction(Request $request, $userID, $shoppingListID)
+  {
+    if($userID<1) {
+      return new JsonResponse(array('error' => 'user'));
     }
-
-    /**
-     * Deletes a shoppingList entity.
-     *
-     * @Route("/{id}", name="shoppinglist_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, ShoppingList $shoppingList)
-    {
-        $form = $this->createDeleteForm($shoppingList);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($shoppingList);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('shoppinglist_index');
+    if($shoppingListID<1) {
+      return new JsonResponse(array('error' => 'shoppinglist'));
     }
-
-    /**
-     * Creates a form to delete a shoppingList entity.
-     *
-     * @param ShoppingList $shoppingList The shoppingList entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(ShoppingList $shoppingList)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('shoppinglist_delete', array('id' => $shoppingList->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
+    $em = $this->getDoctrine()->getManager();
+    $user = $em->getRepository('AppBundle:User')->find($userID);
+    if($user==NULL) {
+      return new JsonResponse(array('error' => 'user'));
     }
+    $shoppingLists = $em->getRepository('AppBundle:ShoppingList')->findByUser($userID);
+    if($shoppingListID>count($shoppingLists)) {
+      return new JsonResponse(array('error' => 'shoppinglist'));
+    }
+    $shoppingList = $shoppingLists[$shoppingListID-1];
+    echo $shoppingList->getId();
+
+    $em->remove($shoppingList);
+    $em->flush();
+
+    return $this->redirectToRoute('shoppinglist_index', array('userID' => $userID));
+  }
 }
