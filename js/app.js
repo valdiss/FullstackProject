597272004c266eb18fb13daa543745cd0d2f7a4d
smontel <montelsylvain@gmail.com>
2017-05-19 11:33:48 +0200

    commentaire + method post au click

--- js/app.js
+++ js/app.js
@@ -8,14 +8,6 @@ shoppingList.controller('shoppingListController', function shoppingListControlle
     url: '/someUrl'
   };
   //configuration des parametres la methode post
-  var post = {
-    method: 'POST',
-    url: '/someUrl',
-    data: {}
-  }
-
-
-
 
   $scope.list = [
     { article: "Book", quantity: 55, bought: false, action: "create" },
@@ -23,6 +15,9 @@ shoppingList.controller('shoppingListController', function shoppingListControlle
     { article: "Book", quantity: 55, bought: false, action: "create" },
     { article: "Book", quantity: 55, bought: false, action: "create" }
   ];
+
+
+    
 //fonction de reset des champs du formulaire
   $scope.reset = function(){
 
@@ -30,16 +25,26 @@ shoppingList.controller('shoppingListController', function shoppingListControlle
     $scope.newItem.quantity = '';
   };
 
+
+
+//fonction qui s'applique a la validation du formulaire
   $scope.create = function(){
-    newItem = {
+    //récupération des valeurs mises dans le formulaire, et transformation sous forme de JSON
+    newItem = JSON.stringify({
       article: $scope.newItem.article,
       quantity: $scope.newItem.quantity,
       bought: false,
       action: 'create'
-    }
-    console.log(newItem);
+    });
+    //reset des valeurs dans les champs du formulaire
     $scope.newItem.article = '';
     $scope.newItem.quantity = '';
+    //post de l'item créé à la base de donnée
+    $http.post(posturl,newItem).then(function successCallback(response){
+      console.log('post success : '+ response);
+    },function errorCallback(response){
+      console.log('error on the post method');
+    })
   };
 
 
@@ -56,6 +61,8 @@ shoppingList.controller('shoppingListController', function shoppingListControlle
       // or server returns response with an error status.
     })
   };
+
+
 
   //Appel de getList au chargement du DOM
   angular.element(document).ready(function() {
